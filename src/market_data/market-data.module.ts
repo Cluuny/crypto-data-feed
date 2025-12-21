742fb1f84d9f8e52482d0163037028d0ae398073
Vicente Matallana <vistrent834@gmail.com>
2025-12-21 00:40:15 -0500

    chore: cambios minimos en el modulo principal

--- src/market_data/market-data.module.ts
+++ src/market_data/market-data.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IngestMarketDataUseCase } from './application/use-cases/ingest-market-data.use-case';
-import { BinanceWsAdapter } from './infrastructure/adapters/exchanges/binance-ws.adapter';
-// import { CoinbaseWsAdapter } from './infrastructure/adapters/exchanges/coinbase-ws.adapter';
+import { BackfillMarketDataUseCase } from './application/use-cases/backfill-market-data.use-case';
+import { BinanceWsAdapter } from './infrastructure/adapters/exchanges/ws/binance-ws.adapter';
+// import { BybitWsAdapter } from './infrastructure/adapters/exchanges/coinbase-ws.adapter';
 import { RedisStreamAdapter } from './infrastructure/adapters/redis/redis-stream.adapter';
 import { StreamPublisherPort } from './domain/ports/out/stream-publisher.port';
 import { MarketHistoryRepositoryPort } from './domain/ports/out/market-history-repository.port';
 import { TypeOrmMarketHistoryRepository } from './infrastructure/adapters/persistence/typeorm-market-history.repository';
 import { PriceTickEntity } from './infrastructure/adapters/persistence/entity/typeorm-tick.entity';
+import { BinanceRestClientAdapter } from './infrastructure/adapters/exchanges/BinanceRestClientAdapter';
+import dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
   imports: [TypeOrmModule.forFeature([PriceTickEntity])],
   providers: [
@@ -22,6 +26,18 @@ import { PriceTickEntity } from './infrastructure/adapters/persistence/entity/ty
       provide: 'EXCHANGE_CONNECTORS',
       useFactory: () => {
         return [new BinanceWsAdapter()];
+      },
+    },
+    {
+      provide: MarketHistoryRepositoryPort,
+      useClass: TypeOrmMarketHistoryRepository,
+    },
+    BackfillMarketDataUseCase,
+    // Proveedor para Binance
+    {
+      provide: 'EXCHANGE_REST_CLIENTS',
+      useFactory: () => {
+        return [new BinanceRestClientAdapter()];
       },
     },
     {
